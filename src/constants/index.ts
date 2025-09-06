59f0d5cf2b9212d1dd6c939bda9886734e21c224
khumora77 <xumora427@gmail.com>
2025-09-06 23:04:50 +0500

    images

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -36,24 +36,24 @@ export const messages = [
 ] as const;
 
 export const cards = [
-  { id: "01", image: "/src/assets/teachers/image.png" },
-  { id: "03", image: "/src/assets/teachers/image copy 2.png" },
-  { id: "04", image: "/src/assets/teachers/image copy 3.png" },
-  { id: "05", image: "/src/assets/teachers/image copy 4.png" },
-  { id: "06", image: "/src/assets/teachers/image copy 5.png" },
-  { id: "07", image: "/src/assets/teachers/image copy 6.png" },
-  { id: "08", image: "/src/assets/teachers/image copy 7.png" },
-  { id: "09", image: "/src/assets/teachers/image copy 8.png" },
+  { id: "01", image: "/teachers/image.png" },
+  { id: "03", image: "/teachers/image copy 2.png" },
+  { id: "04", image: "/teachers/image copy 3.png" },
+  { id: "05", image: "/teachers/image copy 4.png" },
+  { id: "06", image: "/teachers/image copy 5.png" },
+  { id: "07", image: "/teachers/image copy 6.png" },
+  { id: "08", image: "/teachers/image copy 7.png" },
+  { id: "09", image: "/teachers/image copy 8.png" },
 ] as const;
 
 export const pupils = [
-  { id: "01", image: "/src/assets/pupils/image.png" },
-  { id: "02", image: "/src/assets/pupils/image copy.png" },
-  { id: "03", image: "/src/assets/pupils/image copy 9.png" },
-  { id: "04", image: "/src/assets/pupils/image copy 2.png" },
-  { id: "05", image: "/src/assets/pupils/image copy 3.png" },
-  { id: "06", image: "/src/assets/pupils/image copy 4.png" },
-  { id: "07", image: "/src/assets/pupils/image copy 5.png" },
+  { id: "01", image: "/pupils/image.png" },
+  { id: "02", image: "/pupils/image copy.png" },
+  { id: "03", image: "/pupils/image copy 9.png" },
+  { id: "04", image: "/pupils/image copy 2.png" },
+  { id: "05", image: "/pupils/image copy 3.png" },
+  { id: "06", image: "/pupils/image copy 4.png" },
+  { id: "07", image: "/pupils/image copy 5.png" },
 ] as const;
 
 export const news = [
@@ -62,35 +62,35 @@ export const news = [
     title: "news1.title1",
     text: "news1.text1",
     link: "https://job.piima.uz/",
-    image: "/src/assets/news/image copy 3.png",
+    image: "/news/image copy 3.png",
   },
   {
     id: "02",
     title: "news1.title2",
     text: "news1.text2",
     link: "https://ariza.piima.uz/application/cabinet",
-    image: "/src/assets/news/image copy 4.png",
+    image: "/news/image copy 4.png",
   },
   {
     id: "03",
     title: "news1.title3",
     text: "news1.text3",
     link: "https://ariza.piima.uz/application/cabinet",
-    image: "/src/assets/news/image.png",
+    image: "/news/image.png",
   },
   {
     id: "04",
     title: "news1.title4",
     text: "news1.text4",
     link: "https://job2.piima.uz/",
-    image: "/src/assets/news/image copy.png",
+    image: "/news/image copy.png",
   },
   {
     id: "05",
     title: "news1.title5",
     text: "news1.text5",
     link: "https://ariza.piima.uz/application/main",
-    image: "/src/assets/news/image copy 2.png",
+    image: "/news/image copy 2.png",
   },
 ];
 
